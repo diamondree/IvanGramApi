59e5d0b27209f979211f866cea0281d523088951
diamondree <kirill00400@gmail.com>
2022-11-03 22:43:45 +0300

    Add project files.

---
+++ IvanGram/Program.cs
@@ -0,0 +1,87 @@
+using IvanGram;
+using IvanGram.Configs;
+using IvanGram.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var authSection = builder.Configuration.GetSection(AuthConfig.Position);
+var authConfig = authSection.Get<AuthConfig>();
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DAL.DataContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSql"), sql => { });
+});
+
+builder.Services.AddAutoMapper(typeof(MapperProfile).Assembly);
+
+builder.Services.AddScoped<UserService>();
+
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(opt =>
+{
+    opt.RequireHttpsMetadata = false;
+    opt.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = authConfig.Issuer,
+        ValidateAudience = true,
+        ValidAudience = authConfig.Audience,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = authConfig.SymmetricSecurityKey(),
+        ClockSkew = TimeSpan.Zero,
+    };
+});
+
+builder.Services.AddAuthorization(opt =>
+{
+    opt.AddPolicy("ValidAcessToken", prop =>
+    {
+        prop.AuthenticationSchemes.Add(JwtBearerDefaults.AuthenticationScheme);
+        prop.RequireAuthenticatedUser();
+    });
+});
+
+
+var app = builder.Build();
+
+
+using (var serviceScope = ((IApplicationBuilder)app).ApplicationServices.GetService<IServiceScopeFactory>()?.CreateScope())
+
+{
+    if (serviceScope != null)
+    {
+        var context = serviceScope.ServiceProvider.GetRequiredService<DAL.DataContext>();
+        context.Database.Migrate();
+    }
+}
+
+
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
